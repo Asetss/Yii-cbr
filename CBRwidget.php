075ba04ad64ebd004d0409e449580fddd0ad5189
Asetss <assets92@gmail.com>
2014-01-19 18:30:14 +0400

    Added files

---
+++ CBRwidget.php
@@ -0,0 +1,52 @@
+<?php
+
+Class CBRwidget extends CWidget {
+
+    public $curs= array();
+    public $listcurs = array();
+	
+    /**
+    * Run widget
+    */
+     
+    public function run()
+    {
+       return self::cbr();
+    }
+
+     /**
+     * Processing currency.
+     * @return string 
+     */
+     
+    public function cbr()
+    {
+		
+	$xml = new DOMDocument();
+        $url = 'http://www.cbr.ru/scripts/XML_daily.asp?date_req=' . date('d.m.Y');
+
+     if ($xml->load($url))
+        {
+ 
+            $documentElement = $xml->documentElement;
+            $items = $documentElement->getElementsByTagName('Valute');
+ 
+            foreach ($items as $item)
+            {
+                $code = $item->getElementsByTagName('CharCode')->item(0)->nodeValue;
+                $curs = $item->getElementsByTagName('Value')->item(0)->nodeValue;
+                $this->listcurs[$code] = floatval(str_replace(',', '.', $curs));
+            }
+ 
+              foreach($this->curs as $curs) {
+		
+		         echo " <br>$curs ".$this->listcurs[$curs];
+			
+	         }
+        } 
+      
+    }
+
+}
+
+?>
---
+++ README.md
@@ -0,0 +1,18 @@
+Yii-cbr Виджет валюты для Yii
+=======
+
+Использование виджета:
+-----
+~~~
+[php]
+     $this->widget('CBRwidget', array('curs'=> array('EUR')));
+~~~
+
+Использование несколько валюты 
+-----
+~~~
+[php]
+     $this->widget('CBRwidget', array('curs'=> array('EUR','USD')));
+~~~
+
+Валюта пишеться по стандарту ISO 4217 
