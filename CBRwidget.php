be6b0cdace63e3a6914a07c5c45b198b961f824d
Asetss <assets92@gmail.com>
2014-01-19 05:33:19 -0800

    Update CBRwidget.php
    change code style

--- CBRwidget.php
+++ CBRwidget.php
@@ -25,24 +25,24 @@ Class CBRwidget extends CWidget {
 	$xml = new DOMDocument();
         $url = 'http://www.cbr.ru/scripts/XML_daily.asp?date_req=' . date('d.m.Y');
 
-     if ($xml->load($url))
-        {
+     if ($xml->load($url)) {
  
             $documentElement = $xml->documentElement;
             $items = $documentElement->getElementsByTagName('Valute');
  
-            foreach ($items as $item)
-            {
+            foreach ($items as $item) {
+            	
                 $code = $item->getElementsByTagName('CharCode')->item(0)->nodeValue;
                 $curs = $item->getElementsByTagName('Value')->item(0)->nodeValue;
                 $this->listcurs[$code] = floatval(str_replace(',', '.', $curs));
+              
             }
  
-              foreach($this->curs as $curs) {
+            foreach($this->curs as $curs) {
 		
-		         echo " <br>$curs ".$this->listcurs[$curs];
+		  echo " <br>$curs ".$this->listcurs[$curs];
 			
-	         }
+	    }
         } 
       
     }
